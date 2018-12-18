5b25e573f32131579e362346f5f3b79727b02aeb
bryneTane <bftane@gmail.com>
2018-12-18 20:26:55 +0100

    tabs, modal

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Tabs } from 'antd-mobile';
+import { Tabs, Button } from 'antd-mobile';
+import { SyncLoader as Loader } from 'halogenium'
 import 'antd-mobile/dist/antd-mobile.css';
 import MangaItem from './components/MangaItem';
 
 const tabs = [
-  { title: 'Votre Météo', sub: '1' },
-  { title: 'Recherche', sub: '2' },
+  { title: 'All Mangas', sub: '1' },
+  { title: 'Should Watch', sub: '2' },
 ];
 
-const TabExample = (props) => (
+const TabElts = (props) => (
   <div>
     <Tabs tabs={tabs}
-      initialPage={1}
+      initialPage={0}
       tabBarPosition="top"
       renderTab={tab => <span className="title">{tab.title}</span>}
     >
-      <div className = "tabContent">
-        
+      <div className="tabContent">
+        {props.mangaElts}
+        <Button className="arbutton" onClick={props.onClickA}> Add </Button>
       </div>
       <div className = "tabContent">
-        
+        {props.selectedMangaElts}
+        <Button className="arbutton" onClick={props.onClickB}> Remove </Button>
       </div>
     </Tabs>
  
@@ -34,25 +37,74 @@ class App extends Component {
   state = {
     isLoading: true,
     mangas : [],
+    selectedMangas: [],
   }
+
+  temp = [];
+  tempSelect = [];
 
   componentWillMount(){
     this.remote();
-    this.setState({isLoading: false});
   }
   
   remote = async() => {
     const api_call = await fetch("https://gist.githubusercontent.com/aws1994/f583d54e5af8e56173492d3f60dd5ebf/raw/c7796ba51d5a0d37fc756cf0fd14e54434c547bc/anime.json");
     const data = await api_call.json();
+    data.forEach(element => {
+      element.checked = false;
+    });
     this.setState({mangas: data});
     console.log(data);
+    this.temp = this.state.mangas;
+    this.tempSelect = [];
+    console.log(this.temp, this.tempSelect);
+    await this.setState({isLoading: false});
     return data;
+  }
+
+  changeStatus(i, tab){
+    tab[i].checked = !tab[i].checked;
+  }
+
+  organize(tab1, tab2, good){
+    tab1.forEach(item => {
+      if(item.checked){
+        item.checked = false;
+        tab2.push(item);
+      }
+    });
+    tab2.forEach(item => {
+      if(tab1.indexOf(item) !== -1){
+        tab1.splice(tab1.indexOf(item), 1);
+      }
+    });
+    good ? this.setState({mangas: tab1, selectedMangas: tab2}) : this.setState({mangas: tab2, selectedMangas: tab1});
   }
 
   render() {
 
-    const mangaElts = this.state.mangas.map( item => {
-        return <MangaItem data={item} />;
+    if(this.state.isLoading){
+      return (
+          <div>
+            <div className='header'>
+              My Manga List
+            </div>
+            <div className="content">
+              <Loader className="load" color="blue"/>
+            </div>
+            <div className='footer'>
+              Copyrigths &#169; Friedrich TANE 
+            </div>
+          </div>
+        );
+    }
+
+    const mangaElts = this.state.mangas.map( (item, index) => {
+        return <MangaItem data={item} key={index} onChange={() => {this.changeStatus(index, this.temp)}} />;
+    });
+
+    const selectedMangaElts = this.state.selectedMangas.map( (item, index) => {
+        return <MangaItem data={item} key={index} onChange={() => {this.changeStatus(index, this.tempSelect)}} />;
     });
 
     return (
@@ -61,7 +113,7 @@ class App extends Component {
           My Manga List
         </div>
         <div className="content">
-          {mangaElts}
+          <TabElts mangaElts={mangaElts} selectedMangaElts={selectedMangaElts} onClickA={() => this.organize(this.temp, this.tempSelect, true)} onClickB={() => this.organize(this.tempSelect, this.temp, false)} />
         </div>
         <div className='footer'>
           Copyrigths &#169; Friedrich TANE 
--- src/components/MangaItem.js
+++ src/components/MangaItem.js
@@ -1,17 +1,52 @@
 import React, { Component } from 'react';
-import { Checkbox } from 'antd-mobile';
+import { Checkbox, Modal } from 'antd-mobile';
 import '../css/MangaItem.css';
 
 export default class MangaItem extends Component{
 
+    state = {modal1: false}
+
+    onClose = () => {
+        this.setState({
+          modal1: false,
+        });
+      }
+
+      showModal = (e) => {
+        e.preventDefault(); 
+        this.setState({
+          modal1: true,
+        });
+        console.log("Bouh");
+      }
    
     render(){
 
         const CheckboxItem = Checkbox.CheckboxItem;
+        const modal =   <Modal
+                            visible={this.state.modal1}
+                            transparent
+                            maskClosable={false}
+                            onClose={() => this.onClose()}
+                            title={this.props.data.name}
+                            footer={[{ text: 'Ok', onPress: () => { this.onClose(); } }]}
+                        >
+                            
+                            <div className="modalContent">
+                                <div><img src={this.props.data.img} alt={this.props.data.name} className="image" /></div>
+                                <p><b>Description : </b> {this.props.data.description} </p>
+                                <p><b>Rating : </b> {this.props.data.Rating} </p>
+                                <p><b>Episodes : </b> {this.props.data.episode} </p>
+                                <p><b>Category : </b> {this.props.data.categorie} </p>
+                                <p><b>Studio : </b> {this.props.data.studio} </p>
+                            </div>
+
+                        </Modal>
 
         return (
-            <div className="manga">
-                <CheckboxItem className="mangaCheck" >
+            <div className="manga" onClick={this.showModal}>
+                {modal}
+                <CheckboxItem className="mangaCheck" onChange={this.props.onChange} defaultChecked={false} >
                     {this.props.data.name}
                     <img src={this.props.data.img} alt={this.props.data.name} className="image" />
                 </CheckboxItem>
